9e81ba4f945234141d37b2ebd948f005de129c07
Carlos <clopez701256@yahoo.com>
2025-05-17 16:37:25 -0700

    Added kid's chore arrays

--- chore-list/src/components/choreList.jsx
+++ chore-list/src/components/choreList.jsx
@@ -11,12 +11,16 @@ function ChoreList() {
         { name: "Wipe down Countertops" }
     ]);
 
-    const [weelyChores, setWeeklyChores] = useState([
-        { name: "Walk the Dogs" },
-        { name: "Sweep the Kitchen" },
-        { name: "Take out the Trash" },
-        { name: "Wipe down Countertops" }
+    const [weeklyChores, setWeeklyChores] = useState([
+        { name: "Swiffer mop the Kitchen" },
+        { name: "Clean the toilet" },
+        { name: "Vaccuum the Living Room" },
+        { name: "Wipe down the Living Room Table" }
     ]);
+
+    const [joeChores, setJoeChores] = useState([])
+    const [middyChores, setMiddyChores] = useState([])
+
 
     function randomizeChores() {
 
@@ -26,7 +30,7 @@ function ChoreList() {
         <div>
             <Container>
                 <Card>
-                    <Card.Title>Chore List</Card.Title>
+                    <Card.Title>Daily Chores</Card.Title>
                     <Card.Body>
                         <ul>
                             {dailyChores.map((chore) => (
@@ -36,11 +40,29 @@ function ChoreList() {
                     </Card.Body>
                 </Card>
                 <Card>
-                    <Card.Title>Chore List 2</Card.Title>
+                    <Card.Title>Weekly Chores</Card.Title>
                     <Card.Body>
                         <ul>
-                            {dailyChores.map((chore) => (
-                                <li key={chore.id}>{chore.name}</li>
+                            {weeklyChores.map((chore) => (
+                                <li key={chore.name}>{chore.name}</li>
+                            ))}                        </ul>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Title>Middy's Chores</Card.Title>
+                    <Card.Body>
+                        <ul>
+                            {middyChores.map((chore) => (
+                                <li key={chore.name}>{chore.name}</li>
+                            ))}                        </ul>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Title>Joe's Chores</Card.Title>
+                    <Card.Body>
+                        <ul>
+                            {joeChores.map((chore) => (
+                                <li key={chore.name}>{chore.name}</li>
                             ))}                        </ul>
                     </Card.Body>
                 </Card>
